35ca82212a655894ed50679da39906397acf3f7a
Abdul Jalil <abduljaleelmemon3@gmail.com>
2023-08-21 01:59:07 +0500

    added joke repository file and apis endpoints TBD.

--- src/apis/v1.py
+++ src/apis/v1.py
@@ -3,6 +3,21 @@ from fastapi import APIRouter
 v1 = APIRouter(prefix='/v1')
 
 
-@v1.get('/get_random_joke')
+@v1.get('/random')
 async def get_random_joke():
+    return "TBD"
+
+
+@v1.get('/lookup')
+async def get_lookup_joke():
+    return "TBD"
+
+
+@v1.get('/category')
+async def get_joke_from_category():
+    return "TBD"
+
+
+@v1.get('/submit')
+async def add_joke():
     return "TBD"
